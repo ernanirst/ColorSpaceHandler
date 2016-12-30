4416f301100f75aec19578ba75fee7c3094493dc
Ernani <ernanirst@gmail.com>
2016-12-30 00:48:53 -0200

    update info and added small tests

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Java library with a bunch of color space operations
 ## Features
 
 * YUV image handlig;
+* LAB image handlig;
 * Conversion of YUV > RGB > YUV;
+* Conversion of LAB > XYZ > RGB > XYZ > LAB;
+
+## Others
+
+Good testing tool: [Colorizer](http://colorizer.org/) 
--- src/com/uppoints/colorSpace/Main.java
+++ src/com/uppoints/colorSpace/Main.java
@@ -2,6 +2,8 @@ package com.uppoints.colorSpace;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
 import javax.imageio.ImageIO;
 
 public class Main {
@@ -13,6 +15,19 @@ public class Main {
 	 */
 	public static void main(String[] args) throws Exception {
        
+		testYUVcreateBlackImage();
+		//testYUVcreateImageFromFile();
+		
+		//testLABcreateBlackImage();
+		//testLABcreateImageFromFile();
+		
+    }
+	
+	/**
+	 * Creates YUV image from file, converts to RGB and save it
+	 * @throws IOException
+	 */
+	public static void testYUVcreateImageFromFile() throws IOException{
 		File file = new File("/home/krashnovr/Imagens/web-dev-meme.jpeg") ;
 		
 		BufferedImage img = ImageIO.read(file) ;
@@ -20,6 +35,38 @@ public class Main {
 		ImageYUV imageYUV = new ImageYUV(img) ;
 		
 		ImageIO.write(imageYUV.getBufferedImage(), "PNG", new File("/tmp/test-save.png")) ;
+	}
+	
+	/**
+	 * Creates YUV image with black color, converts to RGB and save it
+	 * @throws IOException 
+	 */
+	public static void testYUVcreateBlackImage() throws IOException{
+		ImageYUV imageYUV = new ImageYUV(128, 128) ;
+		ImageIO.write(imageYUV.getBufferedImage(), "PNG", new File("/tmp/test-save.png")) ;
+	}
+	
+	/**
+	 * Creates LAB image from file, converts to RGB and save it
+	 * @throws IOException
+	 */
+	public static void testLABcreateImageFromFile() throws IOException{
+		File file = new File("/home/krashnovr/Imagens/web-dev-meme.jpeg") ;
 		
-    }
+		BufferedImage img = ImageIO.read(file) ;
+		
+		ImageLAB imageLAB = new ImageLAB(img) ;
+		
+		ImageIO.write(imageLAB.getBufferedImage(), "PNG", new File("/tmp/test-save.png")) ;
+	}
+	
+	/**
+	 * Creates LAB image with black color, converts to RGB and save it
+	 * @throws IOException 
+	 */
+	public static void testLABcreateBlackImage() throws IOException{
+		ImageLAB imageLAB = new ImageLAB(128, 128) ;
+		ImageIO.write(imageLAB.getBufferedImage(), "PNG", new File("/tmp/test-save.png")) ;
+	}
+	
 }
